43ab188b00a13211cac174eb506c5ac4e3902ee7
Killian356 <killian7195@gmail.com>
2022-07-28 11:40:54 -0500

    added functions to game.js for gameplay features

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,10 +16,30 @@ var enemyHealth = 50;
 
 var enemyAttack = 12;
 
+// Create Function Declaration
 
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!")
+
+// Subtract the value of "playerAttack" from the value of "enemyHealth" and use that result to update the value in the "enemyHealth" Variable
+
+// Log a resulting message to the console so we know that it worked.
+
+console.log(
+    playerName + "attacked" + enemyName + "." + enemyName + "now has" + enemyHealth + "health remaining."
+)
+
+// Subtract the value of "enemyAttack" from the value of "playerHealth" and use that result to update the value in the "playerHealth" Variable.
+
+playerHealth = playerHealth -enemyAttack;
+
+// Log a resulting message to the console so we know that it worked.
+
+enemyName = "attcked" + playerName + "." + playerName + "now has" + playerHealth + "health remaining."
+
 };
+
+// Execute Function Declaration
 
 fight();
 
