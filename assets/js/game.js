5951b3e8ca47d52335ab8f8cfb5e4938be9ea9ef
Killian356 <killian7195@gmail.com>
2022-08-17 12:06:21 -0500

    adding check robot health

--- assets/js/game.js
+++ assets/js/game.js
@@ -29,9 +29,19 @@ console.log(
     playerName + "attacked" + enemyName + "." + enemyName + "now has" + enemyHealth + "health remaining."
 )
 
+// check enemy's health
+
+if (enemyHealth <=0) {
+    window.alert(enemyName + "has died!");
+}
+
+else {
+    window.alert(enemyName + "sill has" + "health left.")
+}
+
 // Subtract the value of "enemyAttack" from the value of "playerHealth" and use that result to update the value in the "playerHealth" Variable.
 
-playerHealth = playerHealth -enemyAttack;
+playerHealth = playerHealth - enemyAttack;
 
 // Log a resulting message to the console so we know that it worked.
 
